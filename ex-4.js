436349ab05c42a1878d3bf99ec8403ba949947f3
AtirujRat <136755600+RattRuj@users.noreply.github.com>
2024-05-15 13:41:41 +0700

    finish exercise

--- ex-4.js
+++ ex-4.js
@@ -3,3 +3,11 @@
 let lightBulbStatus = "On";
 
 //Start coding here
+
+switch (lightBulbStatus) {
+  case "Broken":
+    console.log("Light bulb is Broken.");
+    break;
+  default:
+    console.log("Please choose the correct input (On/Off/Broken)");
+}
